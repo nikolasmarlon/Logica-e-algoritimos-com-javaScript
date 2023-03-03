d5a0595f28d77f074636ecf2958c88771ae76033
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-03-03 10:52:07 -0300

    Rotinas de tratamento de eventos

---
+++ eventos e funcoes/script.js
@@ -0,0 +1,15 @@
+
+//função para mostrar a mensagem
+function mostrarMensagem(){
+    var nome = document.getElementById('nome').value
+    document.getElementById('resposta').textContent = 'Olá ' + nome
+}
+
+//referencia ao elemento que vai chamr a função (mostrarMensagem())
+var mensagem = document.getElementById('mostrar')
+
+//chama a função ao disparar o evento click **OBs. nao por parenteses, pois chama a função antes de disparar o evento
+mensagem.onclick = mostrarMensagem
+
+
+//Essa abordagem é conhecida como rotina de tratamento de eventos DOM. (Document Object Model)
